70670d0ca5a76d1a2150cfa99c3b00b4e0fa3e45
Ryan-rc-Carter <33025230+Ryan-rc-Carter@users.noreply.github.com>
2018-10-02 11:50:21 +0100

    Started Unity Vector tests

---
+++ Unity files/Vectors/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    private Transform playerTransform;
+
+    private Rigidbody playerRigidBody;
+
+    [SerializeField]
+    [Range(0.1f,200.0f)]
+    private float speed = 1.0f;
+
+
+	// Use this for initialization
+	void Start () {
+
+        playerTransform = transform;
+        playerRigidBody = playerTransform.transform.GetComponent<Rigidbody>();
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate() {
+
+      
+        //playerTransform.position = playerTransform.position + playerTransform.forward * speed * Time.deltaTime;
+
+
+
+
+        //playerRigidBody.velocity = playerTransform.forward * speed * Time.deltaTime;
+        playerRigidBody.AddForce(playerTransform.forward * speed);
+	}
+
+         
+}
---
+++ Unity files/Vectors/Assets/Scripts/playerMovementControls.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovementControls : MonoBehaviour {
+
+    private Transform playerTransform;
+
+    private Rigidbody playerRigidbody;
+
+    [SerializeField]
+    [Range(1.0f, 100.0f)]
+    private float speed;
+
+	// Use this for initialization
+	void Start () {
+        playerTransform = transform;
+        playerRigidbody = playerTransform.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+       
+		
+	}
+}
