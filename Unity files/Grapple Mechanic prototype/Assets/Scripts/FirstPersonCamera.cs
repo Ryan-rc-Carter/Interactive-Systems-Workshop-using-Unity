d45cae6cd80326db9a2da94e8132eac27ff4e6f4
Ryan-rc-Carter <33025230+Ryan-rc-Carter@users.noreply.github.com>
2018-10-08 15:56:55 +0100

    Prototype unity file created

---
+++ Unity files/Grapple Mechanic prototype/Assets/Scripts/FirstPersonCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonCamera : MonoBehaviour {
+
+
+    [SerializeField]
+    private Transform target;
+
+    [SerializeField]
+    private float smoothing = 100.0f;
+
+
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        Vector3 newPos = new Vector3(target.position.x, target.position.y, target.position.z);
+        transform.position = Vector3.Lerp(transform.position, newPos, (smoothing * 0.001f));
+
+        
+	}
+}
---
+++ Unity files/Grapple Mechanic prototype/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    private Transform playerTransform;
+
+    private Rigidbody playerRigidbody;
+
+    private bool canJump = true;
+
+
+    [SerializeField]
+    [Range(0.0f, 5.0f)]
+    private float jumpWait = 3.0f;
+
+
+    [SerializeField]
+    [Range(1.0f,100.0f)]
+    private float speed = 5;
+
+    [SerializeField]
+    [Range(1.0f, 100.0f)]
+    private float jumpForce = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+
+        playerRigidbody = GetComponent<Rigidbody>();
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        float x = Input.GetAxis("Horizontal") * speed;
+        float z = Input.GetAxis("Vertical") * speed;
+        bool jump = Input.GetKeyDown("space");
+
+        if ((jump == true) && (canJump = true))
+        {
+            canJump = false;
+            playerRigidbody.AddForce(x, jumpForce, z, ForceMode.Impulse);
+
+            Invoke("JumpWait", jumpWait);
+        }
+
+        playerRigidbody.AddForce(x, 0.0f, z);
+
+
+    }
+
+    private bool JumpWait()
+    {
+        canJump = true;
+        return canJump;
+    }
+}
