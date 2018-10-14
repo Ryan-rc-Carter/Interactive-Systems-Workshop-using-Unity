36896bd4e6e32f3482e9fc34e1324bf7900b308d
Ryan-rc-Carter <33025230+Ryan-rc-Carter@users.noreply.github.com>
2018-10-14 23:43:58 +0100

    testing spring joint

---
+++ Unity files/Grapple Mechanic prototype/Assets/Scripts/GrappleLatch.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrappleLatch : MonoBehaviour {
+
+    public Transform anchorPoint;
+
+    private Vector3 playerNode;
+
+    
+
+	// Use this for initialization
+	void Start () {
+
+        playerNode = anchorPoint.position;
+
+        GetComponent<SpringJoint>().connectedBody.position = playerNode;
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+	}
+}
