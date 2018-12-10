107580f3c08a65c25565722946ac5dccc3730648
Ryan-rc-Carter <33025230+Ryan-rc-Carter@users.noreply.github.com>
2018-12-10 16:29:24 +0000

    Revert "Test"
    This reverts commit 3774dc5ce98b2478aefecc5f09bcf4736b0eecd4.

---
+++ Unity files/Grapple Mechanic prototype/Assets/Scripts/Follow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour
+{
+    public Transform player;
+    private Transform self;
+    private float selfX, selfY;
+
+	// Use this for initialization
+	void Start () {
+
+        self = transform;
+        float selfX = self.position.x;
+        float selfY = self.position.y;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+       
+
+       transform.LookAt(player);
+	}
+}
---
+++ Unity files/Grapple Mechanic prototype/Assets/Scripts/Player.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPrototype : MonoBehaviour
+{
+    private bool isInRange = false;
+    private bool isConnected = false;
+    private Transform m_colliderTransform;
+    private Rigidbody m_colliderRigid;
+    private HingeJoint hinge;
+
+    // Use this for initialization
+    void Start ()
+    {
+        //
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if(!isConnected)
+                Connect();
+            
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            if(isConnected)
+                DisConnect();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        m_colliderTransform = other.GetComponent<Transform>();
+        m_colliderRigid = other.GetComponent<Rigidbody>();
+        m_colliderRigid.isKinematic = false;
+
+        isInRange = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        isInRange = false;
+        m_colliderRigid.isKinematic = true;
+    }
+
+    private void Connect()
+    {
+        if (!isInRange)
+        {
+            Debug.Log("NOT In range");
+            return;
+        }
+
+        Debug.Log("In range and trigerred");
+        isConnected = true;
+        gameObject.AddComponent<HingeJoint>();
+        hinge = transform.GetComponent<HingeJoint>();
+
+        hinge.connectedBody = m_colliderRigid;
+        hinge.enablePreprocessing = false;
+    }
+
+    private void DisConnect()
+    {
+        isConnected = false;
+        Destroy(transform.GetComponent<HingeJoint>());
+    }
+}
